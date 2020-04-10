2fdc63cd2b4c04a9d8367061c8d7c47522841886
Thomas Schneider <tschneit717@gmail.com>
2020-04-09 22:22:53 -0400

    Added a tilt affect to the big yikes

--- src/app/components/BigYikes.vue
+++ src/app/components/BigYikes.vue
@@ -1,16 +1,28 @@
 <template>
-  <div>
-    <p>That's gonna be a big</p>
-    <h1>YIKES</h1>
-    <p>from me dog</p>
+  <div id="app" @mousemove="getCoordinates">
+    <div id="tilt-container" :style="{ perspectiveOrigin: `${x}% ${y}%`}">
+        <p>That's gonna be a big</p>
+        <h1 class="square">YIKES</h1>
+        <p>from me dog</p>
+    </div>
   </div>
 </template>
 <script>
   export default {
     name: "BigYikes",
+    data() {
+      return {
+        x: 0,
+        y: 0
+      }
+    },
     methods: {
       init() {
 
+      },
+      getCoordinates(e) {
+        this.x = e.clientX / 10;
+        this.y = e.clientY / 10;
       }
     },
     mounted() {
@@ -19,17 +31,33 @@
   }
 </script>
 <style lang="scss" scoped>
-  div {
-    width: 100%;
-    height: 100%;
-    position: absolute;
+  #app {
+    width: 100vw;
+    height: 100vh;
     display: flex;
-    align-items: center;
     justify-content: center;
-    flex-direction: column;
   }
   h1 {
     font-size: 200px;
     font-family:fantasy;
+  }
+  #tilt-container {
+    position: relative;
+    height: 100%;
+    width: 60%;
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    perspective: 200px;
+    transform: translateZ(0);
+    backface-visibility: hidden;
+  }
+  .square {
+    width: 100%;
+    text-align: center;
+    position: relative;
+    transform: translateY(1px) rotateX(1deg);
   }
 </style>
