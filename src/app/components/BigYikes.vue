f99aa56191c7212ad691549b4da633eb46dd68e4
Thomas Schneider <tschneit717@gmail.com>
2020-04-10 09:07:59 -0400

    further memes

--- src/app/components/BigYikes.vue
+++ src/app/components/BigYikes.vue
@@ -2,9 +2,10 @@
   <div id="app" @mousemove="getCoordinates">
     <div id="tilt-container" :style="{ perspectiveOrigin: `${x}% ${y}%`}">
         <p>That's gonna be a big</p>
-        <h1 class="square">YIKES</h1>
+        <h1  contenteditable data-heading="YIKES" class="square">YIKES</h1>
         <p>from me dog</p>
     </div>
+    <a href="https://github.com/tschneit717" target="_blank"><i class="fab fa-github"></i></a>
   </div>
 </template>
 <script>
@@ -31,15 +32,98 @@
   }
 </script>
 <style lang="scss" scoped>
+  $red: #fe4560;
+  $blue: #98b5cc;
+  $fontFamily: fantasy;
   #app {
     width: 100vw;
     height: 100vh;
     display: flex;
     justify-content: center;
+    overflow: hidden;
+    &:before
+    {
+        content: "";
+        position: absolute;
+        width: 200%;
+        height: 200%;
+        top: -50%;
+        left: -50%;
+        z-index: -1;    
+        background: rgb(255,94,94);
+        background: radial-gradient(circle at 80%, rgba(255,94,94,1) 0%, rgba(212,68,196,1) 50%);
+  -webkit-animation: rotating 3s linear infinite;
+  -moz-animation: rotating 3s linear infinite;
+  -ms-animation: rotating 3s linear infinite;
+  -o-animation: rotating 3s linear infinite;
+  animation: rotating 3s linear infinite;
+    }
+  }
+  @-webkit-keyframes rotating /* Safari and Chrome */ {
+      from {
+        -webkit-transform: rotate(0deg);
+        -o-transform: rotate(0deg);
+        transform: rotate(0deg);
+      }
+      to {
+        -webkit-transform: rotate(360deg);
+        -o-transform: rotate(360deg);
+        transform: rotate(360deg);
+      }
+    }
+    @keyframes rotating {
+      from {
+        -ms-transform: rotate(0deg);
+        -moz-transform: rotate(0deg);
+        -webkit-transform: rotate(0deg);
+        -o-transform: rotate(0deg);
+        transform: rotate(0deg);
+      }
+      to {
+        -ms-transform: rotate(360deg);
+        -moz-transform: rotate(360deg);
+        -webkit-transform: rotate(360deg);
+        -o-transform: rotate(360deg);
+        transform: rotate(360deg);
+      }
+    }
+  p {
+    font-family: $fontFamily;
+    font-size:2vw
   }
   h1 {
-    font-size: 200px;
-    font-family:fantasy;
+    font-size: 60vw;
+    font-family:$fontFamily;
+  }
+
+  h1 {
+    font-family: 'CoreCircus', sans-serif;
+    text-transform: uppercase;
+    font-size: 14vw;
+    text-align: center;
+    line-height: 1;
+    font-weight: normal;
+    margin: 0;
+    color: $red;
+    position: relative;
+    letter-spacing: -0.8rem;
+    text-shadow: -1px -1px 0 white, 1px -1px 0 white, -1px 1px 0 white, 1px 1px 0 white, 1px 0px 0px $blue, 0px 1px 0px $blue, 2px 1px 0px $blue, 1px 2px 0px $blue, 3px 2px 0px $blue, 2px 3px 0px $blue, 4px 3px 0px $blue, 3px 4px 0px $blue, 5px 4px 0px $blue, 4px 5px 0px $blue, 6px 5px 0px $blue, 5px 6px 0px $blue, 7px 6px 0px $blue, 8px 7px 0px white, 9px 8px 0px white;
+    
+    &:before {
+      content: attr(data-heading);
+      position: absolute;
+      padding-top:4px;
+      color: white;
+      font-family: 'CoreCircus2DDot1';
+      text-shadow: none;
+      transform: translateY(-2px);
+    }
+  }
+
+  h1 {
+    &:focus {
+      outline: none;
+    }
   }
   #tilt-container {
     position: relative;
@@ -60,4 +144,15 @@
     position: relative;
     transform: translateY(1px) rotateX(1deg);
   }
+  @font-face {
+	font-family: 'CoreCircus2DDot1';
+	src: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/209981/333BF4_1_0.eot');
+	src: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/209981/333BF4_1_0.eot?#iefix') format('embedded-opentype'), url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/209981/333BF4_1_0.woff2') format('woff2'), url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/209981/333BF4_1_0.woff') format('woff'), url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/209981/333BF4_1_0.ttf') format('truetype');
+}
+
+@font-face {
+	font-family: 'CoreCircus';
+	src: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/209981/333BF4_8_0.eot');
+	src: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/209981/333BF4_8_0.eot?#iefix') format('embedded-opentype'), url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/209981/333BF4_8_0.woff2') format('woff2'), url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/209981/333BF4_8_0.woff') format('woff'), url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/209981/333BF4_8_0.ttf') format('truetype');
+}
 </style>
